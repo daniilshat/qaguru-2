5db95a2b729810d159a1db4b1741b22269176c7c
Daniil Shatukhin <daniilshstukhin@gmail.com>
2022-03-31 00:50:19 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,7 @@ public class Main {
         System.out.println("Hello, World!");
         System.out.println("New line!");
     }
+    public static void main(String[] args) {
+        System.out.println("new met");
+    }
 }
