1350bc5680b61d3b4f75cad092d516c7bd90a7f9
daniilshat <daniilshstukhin@gmail.com>
2022-03-31 00:52:11 +0300

    added another linr

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("New line!");
+        System.out.println("Slow line!");
     }
     public static void main(String[] args) {
         System.out.println("new met");
